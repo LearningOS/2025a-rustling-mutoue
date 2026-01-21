5eba35000d2b01ec7a3d5f6d7581091fca133e88
mutoue <mutoue_w@163.com>
2026-01-21 08:54:10 +0800

    update algorithm9

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,8 +1,7 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,7 +36,9 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.count += 1;
+        self.items.push(value);
+        self.shift_up(self.count);
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +58,48 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        // check all leaf nodes
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+
+        if left > self.count {
+            return 0;
+        }
+        if right > self.count {
+            return left;
+        }
+
+        if (self.comparator)(&self.items[left], &self.items[right]) {
+            left
+        } else {
+            right
+        }
+    }
+
+    fn shift_up(&mut self, idx: usize) {
+        let mut idx = idx;
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent]) {
+                self.items.swap(idx, parent);
+                idx = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn shift_down(&mut self, idx: usize) {
+        let mut idx = idx;
+        while self.children_present(idx) {
+            let target = self.smallest_child_idx(idx);
+            if (self.comparator)(&self.items[target], &self.items[idx]) {
+                self.items.swap(idx, target);
+                idx = target;
+            } else {
+                break;
+            }
+        }
     }
 }
 
@@ -85,7 +126,19 @@ where
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+
+        self.items.swap(1, self.count);
+        let res = self.items.pop();
+        self.count -= 1;
+
+        if self.count > 0 {
+            self.shift_down(1);
+        }
+
+        res
     }
 }
 
